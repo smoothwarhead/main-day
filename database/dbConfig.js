856d25b40bed338a9a527dc30412546e2108f359
smoothwarhead <simeont126@outlook.com>
2022-05-16 02:43:52 -0500

    latest commit

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,27 +1,27 @@
-const mysql = require('mysql');
-require('dotenv').config();
+// const mysql = require('mysql');
+// require('dotenv').config();
 
 
 
-// create connection
-const db = mysql.createConnection({
+// // create connection
+// const db = mysql.createConnection({
     
-    host: '127.0.0.1',
-    user: 'root',
-    password: '1920Nov@1',
-    database: 'theDay'
-})
+//     host: '127.0.0.1',
+//     user: 'root',
+//     password: '1920Nov@1',
+//     database: 'theDay'
+// })
 
 
-//connect to MySql
-db.connect(err => {
-    if(err){
-        throw err
-    }
-    console.log('MYSQL Connected')
-})
+// //connect to MySql
+// db.connect(err => {
+//     if(err){
+//         throw err
+//     }
+//     console.log('MYSQL Connected')
+// })
 
- module.exports = db;
+//  module.exports = db;
 
 
  
